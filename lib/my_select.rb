9873ef68499be1e3cfb087edf3400b5b31b9c781
Abdul <awsayed@alaska.edu>
2019-05-01 02:55:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+
+	return "This block should not run!" if collection.length == 0
+
+	collect = []
+	i = 0
+	while i < collection.length
+		collect << collection[i]
+		i += 1
+	end
+	print collect
+	return collection
+
 end
