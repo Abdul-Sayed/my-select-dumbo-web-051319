7da43e619f42f12d62126a256e4a46b197f1e4f8
Abdul <awsayed@alaska.edu>
2019-05-01 02:56:21 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,13 +2,12 @@ def my_select(collection)
 
 	return "This block should not run!" if collection.length == 0
 
-	collect = []
+	selected = []
 	i = 0
 	while i < collection.length
-		collect << collection[i]
+		selected.push( yield(collection[i]) )
 		i += 1
 	end
-	print collect
 	return collection
 
 end
